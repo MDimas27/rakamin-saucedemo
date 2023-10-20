79b8ebec11235543e15fa9f99c5bbac2c9d247d7
MDimas27 <mdimas.umb@gmail.com>
2023-10-20 23:58:36 +0700

    Membuat maven project

---
+++ saucedemo/src/main/java/com/rakamin/App.java
@@ -0,0 +1,13 @@
+package com.rakamin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
