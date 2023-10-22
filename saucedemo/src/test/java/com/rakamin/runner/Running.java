d47d21cb58b8d6524fd9b097de5b000ff30523e5
MDimas27 <mdimas.umb@gmail.com>
2023-10-23 00:36:57 +0700

    Update all report testing

--- saucedemo/src/test/java/com/rakamin/runner/Running.java
+++ saucedemo/src/test/java/com/rakamin/runner/Running.java
@@ -1,5 +1,16 @@
 package com.rakamin.runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features",
+        glue = "com.rakamin.stepDefinitions",
+        plugin = {"html:target/HTML_report.html", "json:target/JSON_report.json", "junit:target/XML_report.xml"}
+		)
 public class Running {
 
 }
